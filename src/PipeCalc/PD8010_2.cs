45cec632868af3acf4d8c35f9be6c26435f7a615
Stuart Roy <stuartcharlesroy@gmail.com>
2018-03-23 17:21:04 +0000

    initial commit

---
+++ src/PipeCalc/PD8010_2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PipeCalc
+{
+    public class PD8010_2
+    {
+        private double _OD;
+        private double _ID;
+        private double _WT;
+        private bool _ODControlled;
+        private double _internalPressure;   
+        private double _maxExternalPressure;
+        private double _minExternalPressure;
+        private double _yieldStrength;
+        private double _tMinBurst
+
+        public PD8010_2(double diameter, double Pi, double Pe_max, double Pe_min, double yieldStrength, bool isOdControlled = true)
+        {
+            if(isOdControlled)
+            {
+                _OD = diameter;
+            }
+            else
+            {
+                _ID = diameter;
+            }
+            _ODControlled = isOdControlled;
+            _internalPressure = Pi;
+            _maxExternalPressure = Pe_max;
+            _minExternalPressure = Pe_min;
+            _yieldStrength = yieldStrength;
+        }
+
+        private void GetTMinBurst()
+        {
+
+        }
+    }
+}
